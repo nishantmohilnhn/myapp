18be077456ee6ed9929d15fd34ebdafdd31bdf9b
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2022-11-09 23:02:49 +0530

    xoxx

--- src/lib/Components/Component_one.svelte
+++ src/lib/Components/Component_one.svelte
@@ -1,18 +1,42 @@
 <main class="mt-1 ml-0">
-    <div class="flex  overflow-hidden">
+   {#each chart as item}
+    
+    <div class="flex  overflow-hidden pb-1">
         <dir class="">
-            <p class=" text-5xl font-bold text-gray-700 inline-block"> <samp class=" text-purple-600">Project</samp>  <br> Consultant</p>
-               <p> Product Development Support <br>
-                Product Development.
-            </p>
+            <p class=" text-5xl font-bold text-gray-700 inline-block"> <samp class=" text-purple-600">{item.head}</samp> <br> {item.head2}</p>
+               <p> {item.dis} <br> {item.dis2}</p>
         </dir>
         <div class="flex  w-full justify-end items-end ml-1">
-            <div class="relative w-36 h-36 rounded-full flex justify-center items-center text-center p-5 shadow-xl bg-purple-200">
+            <div class="relative w-36 h-36 rounded-full flex justify-center items-center text-center p-5 shadow-md bg-purple-200">
                 <span class="absolute text-8xl left-0 top-0 text-purple-800">"
                 </span>
                 Make it simple, but significant.
              </div>
         </div>
     </div>
-    
+    {/each}
+
 </main>
+
+<script>
+    const chart = [{
+        head:'Project',
+        head2: 'Consultant',
+        dis:'Product Development Support',
+        dis2:'tProduct Developmen',
+    },
+    {
+        head:'marketing',
+        head2: 'Consultant',
+        dis:'Product Development Support',
+        dis2:'tProduct Developmen',
+    },
+    {
+        head:'Product',
+        head2: ' development',
+        dis:'Product Development Support',
+        dis2:'Product Development',
+    },
+    ];
+
+</script>
--- src/lib/Components/Nevbar.svelte
+++ src/lib/Components/Nevbar.svelte
@@ -1,15 +1,35 @@
 <header>
-    <nav class="flex justify-center py-2 px-4 border-b-2 border-purple-300 font-mono font-semibold text-neutral-600	 ">
-        <div class="w-1/2 flex justify-center items-center">
+    <nav class=" flex justify-center py-2 px-4 border-b-2 border-purple-300 font-mono font-semibold text-neutral-600 flex-wrap">
+        <div class="w-1/2  justify-center items-center ">
             <h1 class=" inline-block tracking-tight ">
                <span class=" text-purple-500  font-extrabold inline-block mr-0.5 text-xl"> MS </span>Hygiene Solution
             </h1>
             
         </div>
 
-        <div class="w-1/2 flex justify-end ">
-            <button class=" py-1 px-2 hover:border-b-2 hover:border-purple-500 tracking-tight">Contact Us</button>
-            <button class=" py-1 px-2 hover:border-b-2 hover:border-purple-500">Menu</button>
+        <div class="w-1/2 flex justify-end  font-extrabold">
+            <button class=" py-1 px-2 hover:bg-purple-500 hover:text-white tracking-tight">Contact</button>
+            <button class=" py-1 px-2 hover:bg-purple-500 hover:text-white md:hidden " on:click="{()=>btncic=!btncic}">Menu</button>
+            
         </div>
+        {#if btncic}
+        <div transition:fade class="flex border-t-2 border-purple-300 w-full justify-center">
+            <a href="/" class="inline-block mx-2">Home</a>
+            <a href="/" class="inline-block mx-2">Home</a>
+            <a href="/" class="inline-block mx-2">Home</a>
+            <a href="/" class="inline-block mx-2">Home</a>
+        </div>
+        {/if}
+
+        
     </nav>
+    
+
 </header>
+
+<script>
+	import { fade } from "svelte/transition";
+
+
+    let btncic = false;
+</script>
