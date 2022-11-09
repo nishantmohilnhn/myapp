f312c161d7e317f165e3abeefbad99b8b1feb141
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2022-11-09 23:07:57 +0530

    sdf

--- src/lib/Components/Component_one.svelte
+++ src/lib/Components/Component_one.svelte
@@ -1,12 +1,12 @@
-<main class="mt-1 ml-0">
+<main class="mt-1">
    {#each chart as item}
     
     <div class="flex  overflow-hidden pb-1">
         <dir class="">
             <p class=" text-5xl font-bold text-gray-700 inline-block"> <samp class=" text-purple-600">{item.head}</samp> <br> {item.head2}</p>
-               <p> {item.dis} <br> {item.dis2}</p>
+            <p> {item.dis} <br> {item.dis2}</p>
         </dir>
-        <div class="flex  w-full justify-end items-end ml-1">
+        <div class="flex  w-full justify-end items-end">
             <div class="relative w-36 h-36 rounded-full flex justify-center items-center text-center p-5 shadow-md bg-purple-200">
                 <span class="absolute text-8xl left-0 top-0 text-purple-800">"
                 </span>
