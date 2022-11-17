671b274b0da56bc6998fbc2398c418334c6d17e1
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2022-11-17 22:49:10 +0530

    sklfdjlk sjlkdf🤪

--- src/lib/Components/Hero.svelte
+++ src/lib/Components/Hero.svelte
@@ -18,7 +18,7 @@ class="text-gray-600 body-font bg-purple-100">
 
 {#if vise}
   <div 
-  transition:fly="{{ y: 200, duration: 2000 }}"
+  transition:fly="{{ y: 200, duration: 3000 }}"
   class="container mx-auto flex  px-5 py-24 md:flex-row  flex-col items-center">
       <div class="  lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 class="title-font sm:text-7xl text-3xl mb-4 font-medium text-gray-700"> <span class=" text-purple-500">Project consultancy</span> 
