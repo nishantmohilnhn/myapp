30fa48301e710c2ece1cf4c1299555a22bded1e5
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2022-11-27 22:12:30 +0530

    bhj

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,6 @@
 <script>
-	import ComponentOne from "$lib/Components/Component_one.svelte";
 	import Fotter from "$lib/Components/Fotter.svelte";
-	import Gale from "$lib/Components/Gale.svelte";
 	import Hero from "$lib/Components/Hero.svelte";
-	import Hero2 from "$lib/Components/Hero2.svelte";
-	import Secondseg from "$lib/Components/Secondseg.svelte";
-	import Stats from "$lib/Components/Stats.svelte";
 </script>
 <Hero/>
-<Hero2/>
-<Secondseg/>
 <Fotter/>
