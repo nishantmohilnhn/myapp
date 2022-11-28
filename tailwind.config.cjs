ede24e32ab503783cab4d15715221c2ef86d2f5b
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2022-11-28 21:29:27 +0530

    yoy

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'my-nvy1':'#3F3B6C',
+        'my-nvy2':'#624F82',
+        'my-purp1':'#9F73AB',
+        'my-blue':'#A3C7D6'
+          }
+    },
   },
   plugins: []
 };
