01e40ef96a68e22a9c09aeed3995beca653c182b
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2022-11-29 23:34:59 +0530

    😀

--- src/lib/Components/Hero.svelte
+++ src/lib/Components/Hero.svelte
@@ -1,5 +1,14 @@
+
+
+
+
+
+
+
+
+
 <div class=" bg-my-nvy2 h-screen font-bold">
-  <h1 class=" text-white text-2xl ">Start with Us</h1>
-  <h1 class=" text-white  text-xl">Product Design </h1>
-  <h1 class=" text-white text-xl">Machine Design </h1>
+  <h1 class=" text-white text-2xl "></h1>
+  <h1 class=" text-white text-2xl "></h1>
+  <h1 class=" text-white text-2xl "></h1>
 </div>
--- src/lib/Components/Nevbar.svelte
+++ src/lib/Components/Nevbar.svelte
@@ -6,7 +6,7 @@
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-8 h-8 text-white p-2 bg-purple-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg>
-      <h1 class=" font-semibold">DiaperExpert</h1>
+      <h1 class=" font-semibold">DiaperExperts</h1>
     </div>
     <!--logo and name-->
    <div class="sm:hidden">
@@ -23,9 +23,9 @@
 <div class:hidden class="sm:flex sm:gap-1">
   <a on:click={() => (hidden = !hidden)} class="block ml-2 mt-1 px-1 hover:bg-purple-100 rounded-md font-normal border-b-2 border-purple-100   " href="/">Home</a>
   <a on:click={() => (hidden = !hidden)} class="block ml-2 mt-1 px-1 hover:bg-purple-100 rounded-md font-normal border-b-2 border-purple-100   " href="/about">Services</a>
-  <a on:click={() => (hidden = !hidden)} class="block ml-2 mt-1 px-1 hover:bg-purple-100 rounded-md font-normal border-b-2 border-purple-100   " href="/">Work With</a>
+  <a on:click={() => (hidden = !hidden)} class="block ml-2 mt-1 px-1 hover:bg-purple-100 rounded-md font-normal border-b-2 border-purple-100   " href="/">Work With Us</a>
   <a on:click={() => (hidden = !hidden)} class="block ml-2 mt-1 px-1 hover:bg-purple-100 rounded-md font-normal border-b-2 border-purple-100   " href="/">About Us</a>
-  <a on:click={() => (hidden = !hidden)} class="block ml-2 mt-1 px-1 hover:bg-purple-100 rounded-md font-semibold border-b-2 border-purple-100 bg-purple-400 text-neutral-50 text-center" href="/">Get in Touch</a>
+  <a on:click={() => (hidden = !hidden)} class="block ml-2 mt-1 px-1 mb-1 hover:bg-purple-800 rounded-md font-semibold border-b-2 border-purple-100 bg-purple-400 text-neutral-50 text-center" href="/">Get in Touch</a>
 
 </div>
 
