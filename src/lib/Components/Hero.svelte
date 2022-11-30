afb111de2c4e2bfed35bb5d78f1049c337d7b57e
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2022-11-30 20:35:54 +0530

    🍑

--- src/lib/Components/Hero.svelte
+++ src/lib/Components/Hero.svelte
@@ -1,6 +1,6 @@
 <div class="h-screen">
   
-  <div class="h-1/2 bg-my-nvy1">
+  <div class=" bg-my-nvy1">
     {#if ready}
     <div transition:fly="{{delay: 250, x:-200, duration: 4000 }}">
       <p class="pt-16 text-center text-4xl font-extrabold text-white md:text-6xl">Grow your business,<br />Succeed with Us.</p>
@@ -8,7 +8,7 @@
       <p class="text-center text-2xl font-thin text-white">Product Design</p>
       <p class="text-center text-2xl font-thin text-white">Machine Design</p>
     </div>
-    <p class="text-center text-2xl font-thin text-white">And much more.</p>
+    <p class="text-center text-2xl font-thin text-white pb-16">And much more.</p>
     </div>
     {/if}
   </div>
