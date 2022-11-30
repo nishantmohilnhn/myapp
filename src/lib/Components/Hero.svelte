b7d881411baf7c6cdd3b5bd6c9f4063d689e158b
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2022-11-30 17:51:28 +0530

    more refind oil

---
+++ src/lib/Components/Hero.svelte
@@ -0,0 +1,6 @@
+<div class=" h-screen">
+<div class="h-1/2  bg-my-nvy1">
+</div>
+
+
+</div>
--- src/lib/Components/Nevbar.svelte
+++ src/lib/Components/Nevbar.svelte
@@ -3,7 +3,7 @@
 
   <div class="flex items-center justify-between px-2 py-2">
     <div class="flex items-center gap-1">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-purple-500 rounded-full" viewBox="0 0 24 24">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-my-nvy1 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg>
       <h1 class=" font-semibold">DiaperExperts</h1>
@@ -25,7 +25,7 @@
   <a on:click={() => (hidden = !hidden)} class="ml-2 mt-1 block rounded-md border-b-2 border-purple-100 px-1 hover:text-purple-500 sm:border-b-0" href="/services">Services</a>
   <a on:click={() => (hidden = !hidden)} class="ml-2 mt-1 block rounded-md border-b-2 border-purple-100 px-1 hover:text-purple-500 sm:border-b-0" href="/">Work With Us</a>
   <a on:click={() => (hidden = !hidden)} class="ml-2 mt-1 block rounded-md border-b-2 border-purple-100 px-1 hover:text-purple-500 sm:border-b-0" href="/about">About Us</a>
-  <a on:click={() => (hidden = !hidden)} class="mr-1 ml-2 mt-1 block rounded-full border-b-2 border-purple-100 bg-purple-500 px-1 py-1 text-center font-semibold text-neutral-50 hover:bg-purple-800" href="/">Get in Touch</a>
+  <a on:click={() => (hidden = !hidden)} class="mr-1 ml-2 mt-1 block rounded-full border-b-2 border-purple-100 bg-my-nvy1 px-1 py-1 text-center font-semibold text-neutral-50 hover:bg-purple-800" href="/">Get in Touch</a>
 </div>
 
 
